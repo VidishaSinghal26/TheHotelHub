547292fa41571a66d07f95cd64a0ed2b404d1f14
Vidisha Singhal <90644367+VidishaSinghal26@users.noreply.github.com>
2024-03-18 00:54:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,16 @@ app.use(cors(
    }
 ));
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "React app URL"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 api.get("/" , (req, res)=>{
    res.json("hello");
 })
